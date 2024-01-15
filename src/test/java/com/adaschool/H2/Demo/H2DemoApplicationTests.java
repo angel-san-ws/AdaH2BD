c410f520c9352aa0d54790612943503d92f2542f
Angel Sánchez <anghelo@gmail.com>
2024-01-15 10:57:45 -0600

    Commit inicial

---
+++ src/test/java/com/adaschool/H2/Demo/H2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adaschool.H2.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
